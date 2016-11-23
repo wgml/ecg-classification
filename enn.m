66d02f555dd03dd18d139098876f2b6558919fff
kmbm <mazur.krzysztof.pl@gmail.com>
2016-11-23 22:21:53 +0100

    backup enn p.2

--- enn.m
+++ enn.m
@@ -1,9 +1,6 @@
-function [ result ] = enn( train_data, classes, test_data, num_neighbours, dist_fcn )
-
+function [ class ] = enn( train_data, classes, test_probe, num_neighbours, dist_fcn )
 
 train_amount = size(train_data, 1);
-test_amount = size(test_data, 1);
-result = zeros(test_amount, 1);
 
 if nargin ~= 5
     p = 2;
@@ -13,32 +10,97 @@ end
 uniq_classes = unique(classes);
 uniq_classes_num = length(uniq_classes);
 
+% compute T_i
 T = zeros(uniq_classes_num, 1);
+neighbour_classes_per_node = zeros(train_amount, num_neighbours);
+n_i = zeros(uniq_classes_num, 1);
 for class_idx = 1:uniq_classes_num
    class = uniq_classes(class_idx);
    class_idxs = find(classes == class);
-   n_i = numel(class_idxs);
+   n_i(class_idx) = numel(class_idxs);
    for i = 1:numel(class_idxs)
        idx = class_idxs(i);
        x = train_data(idx, :);
-       train_data_x = [];
-       classes_x = [];
+       train_data_x = zeros(train_amount - 1, length(x));
+       classes_x = zeros(train_amount - 1, 1);
        if (idx > 1)
-           train_data_x = [train_data(1:idx-1, :)];
-           classes_x = [classes(1:idx-1, :)];
+           train_data_x(1:idx-1, :) = train_data(1:idx-1, :);
+           classes_x(1:idx-1) = classes(1:idx-1, :);
        end
        if (idx < train_amount)
-           train_data_x = [train_data_x; train_data(idx+1:end, :)];
-           classes_x = [classes_x; classes(idx+1:end, :)];
+           train_data_x(idx:train_amount - 1, :) = train_data(idx+1:end, :);
+           classes_x(idx:train_amount - 1) = classes(idx+1:end, :);
        end
+       x_nn_classes = n_closest_neighbours(train_data_x, classes_x, x, num_neighbours, dist_fcn);
+       neighbour_classes_per_node(idx, :) = x_nn_classes;
        for r = 1:num_neighbours
-          x_nn_class = knn(train_data_x, classes_x, x, 1, dist_fcn);
-       
-          if (class == x_nn_class)
+          if (class == x_nn_classes(r))
               T(class_idx) = T(class_idx) + 1;
           end
        end
    end
-   T(class_idx) = T(class_idx) / (n_i * num_neighbours);
+   T(class_idx) = T(class_idx) / (n_i(class_idx) * num_neighbours);
 end
-T
+
+% compute delta_n_i^j
+% delta_n(i,j) = delta_n_i^j
+delta_n = zeros(uniq_classes_num);
+for j = 1:uniq_classes_num
+   class = uniq_classes(class_idx);
+   train_data_j = [train_data; test_probe];
+   class_idxs = find(classes == class);
+   classes_j = [classes; class];
+   n_i(class_idx) = numel(class_idxs);
+   neighbour_classes_per_node_j = zeros(train_amount, num_neighbours);
+
+   for i = 1:numel(class_idxs)
+       idx = class_idxs(i);
+       x = train_data_j(idx, :);
+       train_data_x = zeros(train_amount, length(x));
+       classes_x = zeros(train_amount - 1, 1);
+       if (idx > 1)
+           train_data_x(1:idx-1, :) = train_data_j(1:idx-1, :);
+           classes_x(1:idx-1) = classes_j(1:idx-1, :);
+       end
+       if (idx < train_amount)
+           train_data_x(idx:train_amount, :) = train_data_j(idx+1:end, :);
+           classes_x(idx:train_amount) = classes_j(idx+1:end, :);
+       end
+       x_nn_classes = n_closest_neighbours(train_data_x, classes_x, x, num_neighbours, dist_fcn);
+       neighbour_classes_per_node_j(idx, :) = x_nn_classes;
+   end
+   
+   for i = 1:uniq_classes_num
+       for e = 1:numel(class_idxs)
+           n_org = sum(neighbour_classes_per_node(e, :) == class);
+           n_new = sum(neighbour_classes_per_node_j(e, :) == class);
+           
+           if i == j && n_new > n_org
+               delta_n(i, j) = delta_n(i, j) + 1;
+           elseif i ~= j && n_new < n_org
+               delta_n(i, j) = delta_n(i, j) + 1;
+           end
+       end
+   end
+end
+
+% determine result class
+probe_neighbours = n_closest_neighbours(train_data, classes, test_probe, num_neighbours, dist_fcn);
+k_i = zeros(uniq_classes_num, 1);
+for c_idx = 1:uniq_classes_num
+   k_i(c_idx) = sum(probe_neighbours == uniq_classes(c_idx));
+end
+predictions = zeros(uniq_classes_num, 1);
+for j = 1:uniq_classes_num
+    s1 = (delta_n(j, j) + k_i(j) - num_neighbours * T(j)) / ((n_i(j) + 1) / num_neighbours);
+    s2 = 0;
+    for i = 1:uniq_classes_num
+        if i == j
+            continue
+        end
+        s2 = s2 + delta_n(i,j) / (n_i(i) * num_neighbours);
+    end
+    predictions(j) = s1 - s2;
+end
+[~, c_idx] = max(predictions);
+class = uniq_classes(c_idx);
--- knn_script.m
+++ knn_script.m
@@ -3,10 +3,9 @@ clear all;
 
 %%loading data
 %importdata = importdata('./ReferencyjneDane/101/AfterClustering.txt');
-importdata = importdata('./ReferencyjneDane/101/ConvertedQRSRawData.txt');
-load('./ReferencyjneDane/101/Class_IDs.txt');
+importdata = importdata('./ReferencyjneDane/103/ConvertedQRSRawData.txt');
+load('./ReferencyjneDane/103/Class_IDs.txt');
 data=importdata(:,2:18);
-data = data(randperm(size(data, 1)), :);
 %%Normalization
 for j = 1:size(data,2)
     vec = data(:,j);
@@ -21,106 +20,7 @@ train_data = data(1:train_data_amount,:);
 test_data = data((train_data_amount+1):size(data,1),:);
 
 %%
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-% tic()
-% Mdl = fitcknn(train_data, Class_IDs(1:train_data_amount), 'NumNeighbors', 5, 'Distance', 'minkowski', 'Standardize', 0);
-% correct = 0;
-% wrong = 0;
-% classes1 = zeros(size(test_data, 1), 1);
-% for j=1:size(test_data,1)
-%     v = Mdl.predict(data(train_data_amount + j, :));
-%     classes1(j) = v;
-%     if Class_IDs(train_data_amount + j) == v
-%         correct = correct + 1;
-%     else
-%         wrong = wrong + 1;
-%     end
-% end
-% correct_detections_percent1 = correct/(correct+wrong)
-% toc()
-%%
-% tic()
-% classes2 = knn(train_data, Class_IDs(1:train_data_amount), test_data, 5);
-% correct = 0;
-% wrong = 0;
-% for j=1:size(test_data,1)
-%     if Class_IDs(train_data_amount + j) ==  classes2(j)
-%         correct = correct + 1;
-%     else
-%         wrong = wrong + 1;
-%     end
-% end 
-% correct_detections_percent2 = correct/(correct+wrong)
-% classes_missmatch = sum(classes1 ~= classes2) / length(classes1)
-% toc()
-%% enn
-
-train_amount = size(train_data, 1);
-classes = Class_IDs(1:train_data_amount);
-num_neighbours = 5;
-
-% compute T_i
-
-p = 2;
-dist_fcn = @(x, y) sum(abs(x - y) .^ p) ^ (1 / p);
-
-uniq_classes = unique(classes);
-uniq_classes = sort(uniq_classes,1,'ascend');
-uniq_classes_num = length(uniq_classes);
-
-T = zeros(uniq_classes_num, 1);
-neighbour_classes_per_node = zeros(train_amount - 1, num_neighbours);
-n_i = zeros(uniq_classes_num, 1);
-for class_idx = 1:uniq_classes_num
-   class = uniq_classes(class_idx);
-   class_idxs = find(classes == class);
-   n_i(class_idx) = numel(class_idxs);
-   for i = 1:numel(class_idxs)
-       idx = class_idxs(i);
-       x = train_data(idx, :);
-       train_data_x = zeros(train_amount - 1, length(x));
-       classes_x = zeros(train_amount - 1, 1);
-       if (idx > 1)
-           train_data_x(1:idx-1, :) = train_data(1:idx-1, :);
-           classes_x(1:idx-1) = classes(1:idx-1);
-       end
-       if (idx < train_amount)
-           train_data_x(idx:train_amount - 1, :) = train_data(idx+1:end, :);
-           classes_x(idx:train_amount - 1) = classes(idx+1:end);
-       end
-       x_nn_classes = n_closest_neighbours(train_data_x, classes_x, x, num_neighbours, dist_fcn);
-       neighbour_classes_per_node(idx, :) = x_nn_classes;
-       for r = 1:num_neighbours
-          if (class == x_nn_classes(r))
-              T(class_idx) = T(class_idx) + 1;
-          end
-       end
-   end
-   T(class_idx) = T(class_idx) / (n_i(class_idx) * num_neighbours);
-end
-
-%predict classes
-classes3 = zeros(size(test_data, 1), 1);
-for e = 1:size(test_data, 1)
-    tic()
-    Class_IDs(train_data_amount + e)
-    c = enn(train_data, Class_IDs(1:train_data_amount), test_data(e,:),num_neighbours,neighbour_classes_per_node,T,n_i)
-    classes3(e) = c;
-    toc()
-end
-=======
-=======
->>>>>>> parent of 4479a0e... enn p.2
-=======
->>>>>>> parent of 4479a0e... enn p.2
-=======
->>>>>>> parent of 4479a0e... enn p.2
-=======
->>>>>>> parent of 4479a0e... enn p.2
+tic()
 Mdl = fitcknn(train_data, Class_IDs(1:train_data_amount), 'NumNeighbors', 5, 'Distance', 'minkowski', 'Standardize', 0);
 correct = 0;
 wrong = 0;
@@ -135,8 +35,9 @@ for j=1:size(test_data,1)
     end
 end
 correct_detections_percent1 = correct/(correct+wrong)
-
+toc()
 %%
+tic()
 classes2 = knn(train_data, Class_IDs(1:train_data_amount), test_data, 5);
 correct = 0;
 wrong = 0;
@@ -148,25 +49,16 @@ for j=1:size(test_data,1)
     end
 end
 correct_detections_percent2 = correct/(correct+wrong)
-
-%% diff
 classes_missmatch = sum(classes1 ~= classes2) / length(classes1)
-
+toc()
 %% enn
-classes3 = enn(train_data, Class_IDs(1:train_data_amount), test_data, 5);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 4479a0e... enn p.2
-=======
->>>>>>> parent of 4479a0e... enn p.2
-=======
->>>>>>> parent of 4479a0e... enn p.2
-=======
->>>>>>> parent of 4479a0e... enn p.2
-=======
->>>>>>> parent of 4479a0e... enn p.2
+classes3 = zeros(size(test_data, 1), 1);
+for e = 1:size(test_data, 1)
+    tic()
+    c = enn(train_data, Class_IDs(1:train_data_amount), test_data(e,:), 5)
+    classes3(e) = c;
+    toc()
+end
 correct = 0;
 wrong = 0;
 for j=1:size(test_data,1)
