0f58beb351e1b76337e245a7722ee8fc39653ad5
Wojtek Gumuła <voodkapl@gmail.com>
2017-01-10 22:41:39 +0100

    Parallelism fun!

--- cpp/knn-ecg/algorithm/KNN.cpp
+++ cpp/knn-ecg/algorithm/KNN.cpp
@@ -3,35 +3,36 @@
 #include <igl/sort.h>
 
 void KNN::train(const KNN::DataType &train_data, const KNN::LabelType &train_labels) {
-	this->train_data = train_data;
-	this->train_labels = train_labels;
+  this->train_data = train_data;
+  this->train_labels = train_labels;
 }
 
 void KNN::classify(const KNN::DataType &test_data, KNN::LabelType &result) const {
-	long test_amount = test_data.rows();
-	if (test_amount == 0)
-		return;
-	result.resize(test_amount, Eigen::NoChange);
+  long test_amount = test_data.rows();
+  if (test_amount == 0)
+    return;
+  result.resize(test_amount, Eigen::NoChange);
 
-	DistanceType distances{train_data.rows()};
+  DistanceType distances{train_data.rows()};
 
-	for (int sample = 0; sample < test_amount; sample++) {
-		const auto &test_sample = test_data.row(sample);
-		distance(train_data, test_sample, distances);
-		result(sample) = get_mode_from_k_neighbours(distances);
-	}
+  #pragma omp parallel for
+  for (int sample = 0; sample < test_amount; sample++) {
+    const auto &test_sample = test_data.row(sample);
+    distance(train_data, test_sample, distances);
+    result(sample) = get_mode_from_k_neighbours(distances);
+  }
 }
 
 KNN::ClassType KNN::get_mode_from_k_neighbours(KNN::DistanceType &distances) const {
 
-	const auto samples = distances.rows();
-	IndexType indexes{samples};
-	DistanceType distances_sorted{samples};
-	igl::sort(distances, 1, true, distances_sorted, indexes);
-	IndexType k_indexes = indexes.block(0, 0, K, 1);
-	LabelType k_classes{K};
+  const auto samples = distances.rows();
+  IndexType indexes{samples};
+  DistanceType distances_sorted{samples};
+  igl::sort(distances, 1, true, distances_sorted, indexes);
+  IndexType k_indexes = indexes.block(0, 0, K, 1);
+  LabelType k_classes{K};
 
-	for (unsigned int i = 0; i < K; i++)
-		k_classes(i) = train_labels(k_indexes(i)); //todo can be optimized
-	return mode(k_classes);
+  for (unsigned int i = 0; i < K; i++)
+    k_classes(i) = train_labels(k_indexes(i)); //todo can be optimized
+  return mode(k_classes);
 }
